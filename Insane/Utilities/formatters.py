e0013d92310f5aed5f3860be8caa313095500c2b
Devil3420 <105910403+Devil3420@users.noreply.github.com>
2022-05-26 20:19:22 +0530

    Add files via upload

---
+++ Insane/Utilities/formatters.py
@@ -0,0 +1,15 @@
+# Owner @always_hungry365
+# Mayank
+
+
+def bytes(size: float) -> str:
+    """humanize size"""
+    if not size:
+        return ""
+    power = 1024
+    t_n = 0
+    power_dict = {0: " ", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        t_n += 1
+    return "{:.2f} {}B".format(size, power_dict[t_n])
---
+++ Insane/Utilities/theme.py
@@ -0,0 +1,31 @@
+# Owner @always_hungry365
+# Mayank
+
+
+
+
+import random
+
+from Insane.Database import get_theme
+
+themes = [
+    "blue",
+    "black",
+    "red",
+    "green",
+    "grey",
+    "orange",
+    "pink",
+    "yellow",
+]
+
+
+async def check_theme(chat_id: int):
+    _theme = await get_theme(chat_id, "theme")
+    if not _theme:
+        theme = random.choice(themes)
+    else:
+        theme = _theme["theme"]
+        if theme == "Random":
+            theme = random.choice(themes)
+    return theme
